014887d0849a519ae2305ba1d936d9960ec582de
fanyihui <faen_yh@163.com>
2018-01-26 17:16:45 +0800

    add med_order table sql script

--- mdgw/src/main/scripts/fsie_gw_med_orer.sql
+++ mdgw/src/main/scripts/fsie_gw_med_orer.sql
@@ -1,16 +1,14 @@
-DROP TABLE IF EXISTS `med_order`;
-CREATE TABLE `med_order` (
-  `PatientName` varchar(64) DEFAULT NULL,
-  `PatientID` varchar(64) DEFAULT NULL,
-  `PatientGender` char(1) DEFAULT NULL,
-  `PatientBirth` varchar(11) DEFAULT NULL,
-  `Department` varchar(64) DEFAULT NULL,
-  `Room` varchar(20) DEFAULT NULL,
-  `Bed` varchar(20) DEFAULT NULL,
-  `Weight` float DEFAULT NULL,
-  `Height` float DEFAULT NULL,
-  `OrderName` varchar(255) DEFAULT NULL,
-  `PatientType` int(1) DEFAULT NULL,
-  `IsActive` tinyint(1) NOT NULL DEFAULT '1',
-  UNIQUE KEY `UN_PAT_BED` (`Bed`,`IsActive`)
-)
+CREATE TABLE med_order (
+  PatientName varchar(64),
+  PatientID varchar(64),
+  PatientGender char(1),
+  PatientBirth varchar(11),
+  Department varchar(64),
+  Room varchar(20),
+  Bed varchar(20),
+  Weight float,
+  Height float,
+  OrderName varchar(255),
+  PatientType int,
+  IsActive int
+);
