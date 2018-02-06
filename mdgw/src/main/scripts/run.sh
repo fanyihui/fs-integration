5f429b7c9cfdf27b0409a2fe36cd85bbed015d21
fanyihui <faen_yh@163.com>
2018-02-06 11:13:16 +0800

    update run.sh scripts

--- mdgw/src/main/scripts/run.sh
+++ mdgw/src/main/scripts/run.sh
@@ -1,27 +1,26 @@
-export MDGW_HOME="/opt/fsie_mdgw/"
-export JAVA=$JAVA_HOME/bin/java
-export JAVA_OPTS=-Xms512M -Xmx512M
-export LIBPATH=../lib
-export CONFIG = ../conf
-export CP=$CONFIG:
+#!/bin/bash
+
+MDGW_HOME="/opt/mdgw"
+export JAVA_OPTS="-Xms512M -Xmx512M"
+LIBPATH=${MDGW_HOME}/lib
+CONFIG = ${MDGW_HOME}/conf
+CP=${CONFIG}:
 for jarz in $LIBPATH/*.jar
 do CP=$CP:$jarz
 done
-export CP
 
 echo ===============================================================================
-echo.
+echo
 echo Engine Startup Environment
-echo.
-echo JAVA: $JAVA
-echo.
+echo
+echo
 echo CONFIG: $CONFIG
-echo.
+echo
 echo JAVA_OPTS: $JAVA_OPTS
-echo.
+echo
 echo CLASSPATH: $CP
-echo.
+echo
 echo ===============================================================================
-echo.
+echo
 
-$JAVA $JAVA_OPTS -cp $CP com.fs.ie.mdgw.Main
+java $JAVA_OPTS -cp $CP com.fs.ie.mdgw.Main
