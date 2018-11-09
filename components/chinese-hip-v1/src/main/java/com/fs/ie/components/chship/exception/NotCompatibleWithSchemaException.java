c9515e6cc4585355d4592dc206cb8ad3dac5025c
fanyihui <faen_yh@163.com>
2018-11-09 22:25:00 +0800

    add all schemas

---
+++ components/chinese-hip-v1/src/main/java/com/fs/ie/components/chship/exception/NotCompatibleWithSchemaException.java
@@ -0,0 +1,7 @@
+package com.fs.ie.components.chship.exception;
+
+public class NotCompatibleWithSchemaException extends Exception{
+    public NotCompatibleWithSchemaException(String message) {
+        super(message);
+    }
+}
